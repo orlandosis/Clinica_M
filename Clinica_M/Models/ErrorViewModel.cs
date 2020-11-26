139d4d1ef6e7519ed1ec8939e214aa6fe3301c7c
orlandosis26 <orlandosis26@gmail.com>
2020-11-26 11:28:15 -0600

    Agregar archivos de proyecto.

---
+++ Clinica_M/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Clinica_M.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
